14e1e74a4c682503c53c3954958a74f151e6b5db
CarbonFodder <mahkiwi123@gmail.com>
2021-08-18 11:56:41 +0800

    Optimized index script

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,34 @@
-console.log('cum')
+const rotFrameElementId = 'rotating-frame'
 
 function rotateHtml (page) {
-    iframe_target = document.getElementById('rotating-frame')
+    console.log(`Grabbing element: ${rotFrameElementId}`)
 
-    if (page == 'home') {
-        console.log('home')
-        iframe_target.src = './pages/home.html'
-    } else if (page == 'about') {
-        console.log('about')
-        iframe_target.src = './pages/about.html'
-    } else if (page == 'links') {
-        console.log('links')
-        iframe_target.src = './pages/links.html'
-    } else if (page == 'fun') {
-        console.log('fun')
-        iframe_target.src = './pages/fun.html'
-    } else if (page == 'downloads') {
-        console.log('downloads')
-        iframe_target.src = './pages/downloads.html'
+    iframe_target = document.getElementById(rotFrameElementId)
+
+    console.log(`Attempting to get page: ${page}`)
+    switch (page) {
+        case 'home':
+            iframe_target.src = './pages/home.html'
+            console.log('Page grabbed and displayed!')
+            break;
+        case 'about':
+            iframe_target.src = './pages/about.html'
+            console.log('Page grabbed and displayed!')
+            break;
+        case 'links':
+            iframe_target.src = './pages/links.html'
+            console.log('Page grabbed and displayed!')
+            break;
+        case 'fun':
+            iframe_target.src = './pages/fun.html'
+            console.log('Page grabbed and displayed!')
+            break;
+        case 'downloads':
+            iframe_target.src = './pages/downloads.html'
+            console.log('Page grabbed and displayed!')
+            break;
+        default:
+            console.error(`Page not found: ${page}`)
+            break;
     }
 };
